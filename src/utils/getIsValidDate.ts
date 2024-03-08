d7c23bf798c2effbf508445f214c12a002a32c05
Silich Alexander <124524007+sili41995@users.noreply.github.com>
2024-03-08 20:45:21 +0200

    Update getIsValidDate.ts

--- src/utils/getIsValidDate.ts
+++ src/utils/getIsValidDate.ts
@@ -1,9 +1,7 @@
 import { IGetIsValidDateProps } from '../types/types';
 
 const getIsValidDate = ({ year, month }: IGetIsValidDateProps): boolean => {
-  const newDate = new Date();
-  const currentYear = newDate.getFullYear();
-  const validYear = !Number.isNaN(year) && year <= currentYear;
+  const validYear = !Number.isNaN(year);
   const validMonth = !Number.isNaN(month) && month > 0 && month <= 12;
 
   return validYear && validMonth;
