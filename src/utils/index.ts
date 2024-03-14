a0737461b96760f31d8add40f98a7e2f57e5f250
Silich Alexander <124524007+sili41995@users.noreply.github.com>
2024-03-14 20:45:33 +0200

    Update index.ts

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,11 +1,8 @@
 export { default as ctrlWrapper } from './ctrlWrapper';
 export { default as httpError } from './httpError';
-// export { default as sendEmail } from './sendEmail';
 export { default as updateImage } from './updateImage';
 export { default as getImageFilename } from './getImageFilename';
 export { default as getFindFilter } from './getFindFilter';
 export { default as getHashPassword } from './getHashPassword';
-// export { default as getWaterIntakeTime } from './getWaterIntakeTime';
-// export { default as getTodayTime } from './getTodayTime';
 export { default as getFinalDate } from './getFinalDate';
 export { default as getIsValidDate } from './getIsValidDate';
