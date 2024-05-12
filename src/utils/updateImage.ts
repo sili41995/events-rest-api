9cca2fe0c690873d58a04ae8e9e3a86412370f4f
Silich Alexander <124524007+sili41995@users.noreply.github.com>
2024-05-12 11:39:11 +0300

    Update updateImage.ts

--- src/utils/updateImage.ts
+++ src/utils/updateImage.ts
@@ -2,13 +2,13 @@ import { UploadApiResponse } from 'cloudinary';
 import fs from 'fs/promises';
 import cloudinary from './cloudinary';
 import { IUpdateImageProps } from '../types/types';
+import { DefaultAvatarsURL } from '../constants';
 
-const updateImage = async ({
-  path,
-  filename,
-}: IUpdateImageProps): Promise<UploadApiResponse> => {
+const updateImage = async ({ path, filename }: IUpdateImageProps): Promise<UploadApiResponse> => {
+  const isDefaultAvatar = Object.values(DefaultAvatarsURL).some((url) => url.includes(filename));
+
   const result = await cloudinary.uploader.upload(path, {
-    public_id: filename,
+    public_id: isDefaultAvatar ? '' : filename,
     folder: 'avatars',
     width: 200,
     height: 200,
